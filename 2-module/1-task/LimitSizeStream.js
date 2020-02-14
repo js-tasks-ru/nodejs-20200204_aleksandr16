db6f692be3c06405bf0ff012d7e6478cf8040462
AlexanderSamarkin <aleksandr.samarkin@raiffeisen.ru>
2020-02-14 12:03:28 +0300

    added module-2 task-1

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,17 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.transferedBytes = 0;
   }
 
   _transform(chunk, encoding, callback) {
+    this.transferedBytes += chunk.length;
+    if (this.transferedBytes > this.limit) {
+      callback(new LimitExceededError());
+    }
 
+    callback(null, chunk);
   }
 }
 
