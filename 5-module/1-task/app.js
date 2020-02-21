3e9c875f0c205da969687e80b203190d533ada74
AlexanderSamarkin <aleksandr.samarkin@raiffeisen.ru>
2020-02-21 10:08:48 +0300

    koa chat

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,13 +7,49 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let subscribers = {};
 
-router.get('/subscribe', async (ctx, next) => {
-});
+const onSubscribe = async (ctx, next) => {
+  const id = ctx.request.query.r || Math.random();
 
-router.post('/publish', async (ctx, next) => {
-});
+  ctx.set('Content-Type', 'text/plain;charset=utf-8');
+  ctx.set('Cache-Control', 'no-cache, must-revalidate');
+
+  const promise = new Promise((resolve) => {
+    subscribers[id] = resolve;
+
+    ctx.res.on('close', () => {
+      delete subscribers[id];
+    });
+  });
+
+  ctx.body = await promise;
+};
+
+const onPublish = async (ctx, next) => {
+  const message = ctx.request.body.message;
+  if (!message || message === '') {
+    ctx.status = 200;
+    return;
+  }
+
+  for (const i in subscribers) {
+    if (!subscribers.hasOwnProperty(i)) {
+      continue;
+    }
+    subscribers[i](message);
+  }
+
+  subscribers = {};
+  ctx.status = 200;
+};
+
+
+router.get('/subscribe', onSubscribe);
+
+router.post('/publish', onPublish);
 
 app.use(router.routes());
+
 
 module.exports = app;
