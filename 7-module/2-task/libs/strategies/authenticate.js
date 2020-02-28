b9f5863443c4312257125510c5cc19c211e92ac4
AlexanderSamarkin <aleksandr.samarkin@raiffeisen.ru>
2020-02-28 16:03:18 +0300

    07-module 02-task

--- 7-module/2-task/libs/strategies/authenticate.js
+++ 7-module/2-task/libs/strategies/authenticate.js
@@ -1,3 +1,15 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) return done(null, false, 'Не указан email');
+  try {
+    const user = await User.findOne({email});
+    if (user) return done(null, user);
+
+    const oauthUser = new User({email, displayName});
+    await oauthUser.save();
+    return done(null, oauthUser);
+  } catch (e) {
+    return done(e, false, e.message);
+  }
 };
